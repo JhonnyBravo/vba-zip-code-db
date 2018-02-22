a04c8fa2746bdafe12874db47c5b81cc2a648f0c
JhonnyBravo <sanfranceshika5@gmail.com>
2018-02-22 21:18:25 +0900

    modified: sql/selectAddData.sql
    tblUpdateData の構造変更に伴うコード修正。

--- sql/selectAddData.sql
+++ sql/selectAddData.sql
@@ -1,9 +1,8 @@
-SELECT tblUpdateData.郵便番号,
-       tblUpdateData.[住所-カナ],
-       tblUpdateData.住所,
-       tblUpdateData.更新の表示,
-       tblUpdateData.変更理由,
-       tblUpdateData.登録年月日
+SELECT 郵便番号,
+       [都道府県名-カナ] & [市区町村名-カナ] & [町域名-カナ] AS [住所-カナ], 
+       [都道府県名] & [市区町村名] & [町域名] AS 住所,
+       更新の表示,
+       変更理由,
+       Date() AS 登録年月日
 FROM tblUpdateData
 WHERE 更新の表示=1;
-
